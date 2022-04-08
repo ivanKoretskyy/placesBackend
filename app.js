148125a5899902991f97b0e44d9534484ee9f885
Ivan Koretskyy <Ivan_Koretskyy@bcbsil.com>
2022-04-08 15:11:52 -0500

    Add express and routes for place

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/placesRoutes');
+
+const app = express();
+app.use(placesRoutes);
+
+app.listen(5000);
